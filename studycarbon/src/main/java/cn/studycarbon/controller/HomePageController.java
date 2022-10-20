e72004e6176652feda5fa7855a57423c76df0182
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-10-20 22:10:07 +0800

    主页修改

---
+++ studycarbon/src/main/java/cn/studycarbon/controller/HomePageController.java
@@ -0,0 +1,19 @@
+package cn.studycarbon.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/")
+public class HomePageController {
+    public static Logger logger = LoggerFactory.getLogger(HomePageController.class);
+    @GetMapping
+    public ModelAndView homePage() {
+        logger.debug("called.");
+        return new ModelAndView("index");
+    }
+}
