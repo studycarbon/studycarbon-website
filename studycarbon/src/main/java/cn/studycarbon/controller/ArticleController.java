e49afacc8bd4f2d280197bc444445158e7f6195a
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-10-22 11:36:35 +0800

    1.提取main.css/main.css 2.增加文章视图

--- studycarbon/src/main/java/cn/studycarbon/controller/ArticleController.java
+++ studycarbon/src/main/java/cn/studycarbon/controller/ArticleController.java
@@ -1,4 +1,20 @@
 package cn.studycarbon.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/article")
 public class ArticleController {
+    @GetMapping("publisharticleview")
+    ModelAndView publishArticleView() {
+        return new ModelAndView("front/pages/publish-page");
+    }
+
+    @GetMapping("/articlepageview")
+    ModelAndView articlePageView() {
+        return new ModelAndView("front/pages/articlepage");
+    }
 }
