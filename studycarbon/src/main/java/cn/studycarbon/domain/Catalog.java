e9fb403ae6d6a4a9cd21409e330de874fd77f0d2
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-12-09 09:30:54 +0800

    1.添加分类

--- studycarbon/src/main/java/cn/studycarbon/domain/Catalog.java
+++ studycarbon/src/main/java/cn/studycarbon/domain/Catalog.java
@@ -28,9 +28,10 @@ public class Catalog implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 自增长策略
 	private Long id; // 用户的唯一标识
 
+	// 映射为字段，值不能为空
 	@NotEmpty(message = "名称不能为空")
 	@Size(min=2, max=30)
-	@Column(nullable = false) // 映射为字段，值不能为空
+	@Column(nullable = false)
 	private String name;
  
 	@OneToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
