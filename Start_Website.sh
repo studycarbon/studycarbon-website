da7ee8e73e7c1ebfa36b84731c9bbe089e8e4d9c
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2023-02-24 09:59:42 +0800

    修改linux启动脚本，添加一些建议

--- Start_Website.sh
+++ Start_Website.sh
@@ -1,1 +1,13 @@
 java -jar .\studycarbon\target\studycarbon-0.0.1-SNAPSHOT.jar
+
+
+# 一些可能有用的执行方式
+# 杀死所有java进程
+# killall -9 java
+# 启动文件服务
+# cd mongodb-file-server
+# nohup gradle bootRun &
+
+# 启动网站服务
+# cd ../
+# nohup java -jar studycarbon-0.0.1-SNAPSHOT.jar --server.port=88 &
